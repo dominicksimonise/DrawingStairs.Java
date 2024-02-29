e81977175eec7abbff62615ab87d69fa67a25bfd
Dominick Simonise <150468410+dominicksimonise@users.noreply.github.com>
2024-02-29 07:11:18 -0600

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+
+public static void {
+    public static void main(string[] args) {
+        int nummSteps =5;
+        int stepWidth = 3;
+        for (int i = 0; i < numSteps; i++) {
+            for ( int j = 0 ; j < (i + 1)) * stepWidth; j++) {
+                system.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
